b7b1256038442d948d760eaaad9411c696db6213
rajib <rajib.ghosh2a5@gmail.com>
2026-07-21 12:00:39 +0530

    Design responsive dashboard interface

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Dashboard Loaded Successfully");
+console.log("Dashboard Loaded 🚀");
+
+document
+.getElementById("themeBtn")
+.addEventListener("click",()=>{
+
+alert("Dark Mode will be implemented in Sprint 7!");
+
+});
